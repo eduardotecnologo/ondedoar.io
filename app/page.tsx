81c6505339e894237b8bb4fa7b8420cfd2307500
eduardotecnologo <eduardotecnologo@hotmail.com>
2026-02-28 04:14:05 -0300

    fix: rua

--- app/page.tsx
+++ app/page.tsx
@@ -730,7 +730,7 @@ export default async function Home(props: HomeProps) {
 
           <div className="mt-8">
             <h3 className="text-2xl font-bold text-red-700 mb-4">
-              Mianha rua/casa Precisa de Atenção
+              Minha rua/casa Precisa de Atenção
             </h3>
             <div className="bg-white rounded-3xl border border-red-100 shadow-sm p-3">
               <MapaInterdicoesWrapper interdicoes={interdicoes} />
